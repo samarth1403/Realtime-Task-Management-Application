2cc05221149ede7505b49d376889f757fb0f4fcd
Samarth Parmeshwar Ikkalaki <samarthikkalaki123@gmail.com>
2023-10-23 01:07:37 +0530

    All Web Pages are Decided

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "./Components/MainLayout";
+import CreateTaskPage from "./Pages/CreateTaskPage";
+import DashboardPage from "./Pages/DashboardPage";
+import HomePage from "./Pages/HomePage";
+import MyTaskPage from "./Pages/MyTaskPage";
+import NotificationPage from "./Pages/NotificationPage";
+import SigninPage from "./Pages/SigninPage";
+import SignupPage from "./Pages/SignupPage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/my-tasks" element={<MyTaskPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/create-task" element={<CreateTaskPage />} />
+          <Route path="/notifications" element={<NotificationPage />} />
+        </Route>
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ client/src/Pages/CreateTaskPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateTaskPage = () => {
+  return <div>CreateTaskPage</div>;
+};
+
+export default CreateTaskPage;
