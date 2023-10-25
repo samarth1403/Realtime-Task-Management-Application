9921dcfae61e8faad3aacf7b3d3974fdbc9fe741
Samarth Parmeshwar Ikkalaki <samarthikkalaki123@gmail.com>
2023-10-25 15:38:25 +0530

    Ready for Deployment 1.6

--- server/Middlewares/authMiddleware.js
+++ server/Middlewares/authMiddleware.js
@@ -1,4 +1,4 @@
-import userModel from "../Models/userModel.js";
+import userModel from "../Models/UserModel.js";
 import jwt from "jsonwebtoken";
 
 export const authMiddleware = async (req, res, next) => {
